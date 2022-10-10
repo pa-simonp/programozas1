e426e34df2c8bedce81fac62fa0c857b6750b217
pa-simonp <112852487+pa-simonp@users.noreply.github.com>
2022-10-10 09:35:23 +0200

    Add files via upload

---
+++ 11_12óra.py
@@ -0,0 +1,12 @@
+"""gondolt_szám = 4
+print(type(gondolt_szám))
+tipp=input("Gondoltam egy számra. Tippeld meg! ")
+tipp = int(tipp)
+if tipp == gondolt_szám:
+    print("Ügyes!")
+if tipp > gondolt_szám:
+    print("A tipp nagyobb mint a gondolt szám!")
+if tipp < gondolt_szám:    
+    print("A tipp kisebb mint a gondolt szám!")
+
+print("Pápá") """
