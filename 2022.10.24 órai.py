6f039d74a8d3ea99dd6eea8ba1c3a335091c95f5
pa-simonp <112852487+pa-simonp@users.noreply.github.com>
2022-10-24 09:41:16 +0200

    Add files via upload

---
+++ 2022.10.24 órai.py
@@ -0,0 +1,74 @@
+a = """Lorem ipsum dolor sit amet,
+
+consectetur adipiscing elit,
+
+sed do eiusmod tempor incididunt
+
+ut labore et dolore magna aliqua."""
+print(a)
+a = "Hello, World!"
+
+print(a[1])
+
+print(a[4])
+
+for x in "banana":
+
+  print(x)
+
+for x in "banana":
+
+  print(x,end='')
+
+szam= 0
+for x in "banana":
+
+  print(szam,x,sep="---", end="\n")
+  szam=szam + 1
+
+  a = "Helloooo, World!"
+
+print(len(a)) 
+
+
+txt = "The best things in life are free!"
+
+print("best" in txt) # True
+
+
+
+
+txt = "The best things in life are free!"
+
+if "free" in txt:
+
+  print("Yes, 'free' is present.")
+
+else:
+    print("No, 'free' is present.")
+
+    txt = "The best things in life are free!"
+
+
+if "expensive" not in txt:
+
+  print("No, 'expensive' is NOT present.")
+
+  b = "Hello, World!"
+
+print(b[2:8])  # llo
+
+b = "Hello, World!"
+
+print(b[-5:-2]) # orl
+
+a = "Hello, World!"
+
+print(a.lower())
+
+a = " Hello, World! "
+
+print(a.strip()) # returns "Hello, World!"
+
+szam = 10
+
