d57d4db8cfa6d6ba4a9dac80c49d5e4d3254d15e
pa-simonp <112852487+pa-simonp@users.noreply.github.com>
2023-02-16 08:38:30 +0100

    Add files via upload

---
+++ 44.ora.py
@@ -0,0 +1,19 @@
+
+lista1 = [3, 12, 3, 4, 7, 15, 1]
+lista2 = [2, 4, 6, 3, 12]
+lista3 = [2, 4, 7, 8]
+
+def oszzegzes_tetele(lista_oszegzes):
+    osszesen = 0
+    for szam in lista_osszegzes:
+	      osszesen = osszesen + szam
+
+    print("A listában található összege: ", osszesen)
+
+def eldontes_tetele(lista_eldontes):
+
+
+oszzegzes_tetele(lista1)
+oszzegzes_tetele(lista2)    
+oszzegzes_tetele([10, 20, 30]) 
+eldontes_tetele(lista3)
