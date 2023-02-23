dbbf9b0cf4af1d12c3b358d0545b182936cf2f6f
pa-simonp <112852487+pa-simonp@users.noreply.github.com>
2023-02-23 07:52:39 +0100

    Add files via upload

---
+++ 45. ora Doga.py
@@ -0,0 +1,7 @@
+#Simon Patrik 10D 2.feladat
+szavak = None
+while szavak != '':
+      szavak = input('Adj meg szavakat! Ha kilépnél, a szó helyett csak egy ENTER-t üss! ')    
+      print(szavak)
+print('Program vége!')    
+  
